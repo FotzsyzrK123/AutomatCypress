bf37723207fdb3a1e4d78387b472e9cab0c0729d
FotzsyzrK123 <51365071+FotzsyzrK123@users.noreply.github.com>
2025-05-19 16:38:45 +0200

    Update sprawdzam.cy.js

--- sprawdzam.cy.js
+++ sprawdzam.cy.js
@@ -1,10 +1,10 @@
 describe('Test Application Form on Job Offer Page', () => {
     it('Visits the job offer page, checks for specific texts, and fills in the name field', () => {
         // Odwiedzenie strony oferty pracy
-        cy.visit('https://justjoin.it/job-offer/fluke-corporation-v-v-test-engineer-wroclaw-testing');
+        cy.visit('https://justjoin.it/job-offer/montrose-software-polska-sp-z-o-o-software-tester---paid-internship-krakow-testing');
         
         // Poczekaj, aż pojawi się banner cookies, a następnie zamknij go
-        cy.get('#cookiescript_reject', { timeout: 10000 })
+        cy.get('#cookiescript_reject', { timeout: 20000 })
             .should('be.visible')
             .click({ force: true }); // Wymuszenie kliknięcia przycisku zamykającego banner cookies
         
@@ -23,11 +23,32 @@ describe('Test Application Form on Job Offer Page', () => {
             .should('exist')
             .and('be.visible');
 
+        //Sprawdzenie obecności tekstu "Email"
+        cy.contains('Email')
+            .should('exist')
+            .and('be.visible');
+
         // Wypełnienie pola z placeholder="Enter your first and last name"
         cy.get('input[placeholder="Enter your first and last name"]')
             .should('be.visible')
             .type('Krzysztof Kośka'); // Wpisanie tekstu z polskim znakiem
 
+        //Wypełnienie pola placeholder="Enter your email adress"
+        cy.get('input[placeholder="Enter your email address"]')
+            .should('be.visible')
+            .type('k.koska94@gmail.com');
+
+            cy.get('textarea#message').type('Sent through Cypress\nhttps://www.linkedin.com/in/krzysztof-ko%C5%9Bka-a381251a5/\nhttps://github.com/FotzsyzrK123/AutomatCypress');
+
+        cy.contains('Add document')
+            .should('exist')
+            .should('be.visible');
+
+        
+
+        cy.get('input[type="file"][name="attachment"]').attachFile("KrzysztofKoskaCVITEng.pdf");
+
+        cy.contains('Apply').should('exist').click();
         // Wyświetlenie w konsoli, że wszystkie wymagane teksty zostały znalezione i pole zostało wypełnione
         cy.log('Pole imienia zostało wypełnione tekstem: Krzysztof Kośka');
     });
